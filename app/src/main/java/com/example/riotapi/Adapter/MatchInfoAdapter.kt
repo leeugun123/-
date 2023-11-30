3d4a684ddbb35c3508b7c86241703cf78ec405ae
leeugun123 <leeugun123@naver.com>
2023-11-30 18:06:58 +0900

    아이템 사진 부착

--- app/src/main/java/com/example/riotapi/Adapter/MatchInfoAdapter.kt
+++ app/src/main/java/com/example/riotapi/Adapter/MatchInfoAdapter.kt
@@ -53,11 +53,60 @@ class MatchInfoAdapter(private val matchInfoList : List<MatchDto>) : RecyclerVie
                     .fitCenter()
                     .into(holder.binding.champImg)
 
+                val item0Uri = "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/item/" +  participant.item0 + ".png"
+                val item1Uri = "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/item/" +  participant.item1 + ".png"
+                val item2Uri = "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/item/" +  participant.item2 + ".png"
+                val item3Uri = "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/item/" +  participant.item3 + ".png"
+                val item4Uri = "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/item/" +  participant.item4 + ".png"
+                val item5Uri = "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/item/" +  participant.item5 + ".png"
+                val item6Uri = "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/item/" +  participant.item6 + ".png"
+
+
+                Glide.with(holder.itemView.context)
+                    .load(item0Uri)
+                    .fitCenter()
+                    .into(holder.binding.item0)
+
+                Glide.with(holder.itemView.context)
+                    .load(item1Uri)
+                    .fitCenter()
+                    .into(holder.binding.item1)
+
+                Glide.with(holder.itemView.context)
+                    .load(item2Uri)
+                    .fitCenter()
+                    .into(holder.binding.item2)
+
+                Glide.with(holder.itemView.context)
+                    .load(item3Uri)
+                    .fitCenter()
+                    .into(holder.binding.item3)
+
+                Glide.with(holder.itemView.context)
+                    .load(item4Uri)
+                    .fitCenter()
+                    .into(holder.binding.item4)
+
+                Glide.with(holder.itemView.context)
+                    .load(item5Uri)
+                    .fitCenter()
+                    .into(holder.binding.item5)
+
+                Glide.with(holder.itemView.context)
+                    .load(item6Uri)
+                    .fitCenter()
+                    .into(holder.binding.item6)
+
+
+
                 holder.binding.kill.text = participant.kills.toString()
                 holder.binding.death.text = participant.deaths.toString()
                 holder.binding.assist.text = participant.assists.toString()
 
-
+                /*
+                holder.binding.kda.text = participant.challenges.kda.toString()
+                holder.binding.killParticipate.text = participant.challenges.killParticipation.toString()
+                */
 
             }
 
